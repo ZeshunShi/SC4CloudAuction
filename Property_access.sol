552f0023feeaa00a0f2489d26d4bdd5a479748b3
Zeshun Shi <zeshunshi@wcw-self-145-109-125-253.wireless.uva.nl>
2019-11-22 17:29:12 +0100

    test function access

--- Property_access.sol
+++ Property_access.sol
@@ -2,15 +2,47 @@ pragma solidity ^0.4.4;
 
 // public internal private
 
-contract Person {
+contract Animal {
 
-  uint internal _age;
   uint _weight;
   uint private _height;
+  uint internal _age;
   uint public _money;
 
-  function money() constant returns (uint) {
+  /* function money() constant returns (uint) {
     return 120;
+  } */
+
+  function test() constant returns (int) {
+    return _weight;
+  }
+
+  function test1() constant public returns (int) {
+    return _weight;
+  }
+
+  function test2() constant internal returns (int) {
+    return _weight;
+  }
+
+  function test3() constant private returns (int) {
+    return _weight;
+  }
+
+  function testInternal() constant returns () {
+    return this.test()
+  }
+
+  function testInternal1() constant returns () {
+    return this.test1()
+  }
+
+  function testInternal2() constant returns () {
+    return test2()
+  }
+
+  function testInternal3() constant returns () {
+    return test3()
   }
 
 }
