94ee934afbb5337610ce7d75aadfd4767935d6e5
ZeshunShi <44780304+ZeshunShi@users.noreply.github.com>
2020-07-21 15:43:30 +0200

    Update CloudAuction.sol

--- CloudAuction.sol
+++ CloudAuction.sol
@@ -1,20 +1,47 @@
 pragma solidity ^0.5.0;
-/**
- * The CloudAuction contract is a smart contract on Ethereum that supports the decentralized cloud providers to auction and bid the cloud services (IaaS). 
- * examanier/auditor/arbiter
- */
+    /**
+     * The CloudAuction contract is a smart contract on Ethereum that supports the decentralized cloud providers to auction and bid the cloud services (IaaS). 
+     * examanier/auditor/arbiter
+     */
 
 
-// Some solidity libraries used in this contract.
+// Some imported solidity libraries used in this contract.
 import "./library/librarySorting.sol";
 
 
 contract CloudAuction {
-  constructor() 
+    constructor() 
 		public 
-  {
+    {
+
+    }
+
+
+    enum CloudProviderState { Candidate, Success, Quit,  }
+
+    struct Provider {
+
+        bool registered;    ///true: this provider has registered.
+        uint index; ///the index of the provider in the address pool, if it is registered       
+        int8 reputaion; //the reputation of the provider, the initial value is 0.
+        CloudProviderState state;  // the state of the provider
+        address SLAContract;    ////the SLA contract address of 
+        
+    }
+
+    mapping (address => Provider) providerCrowd;
+
+    // mapping(address => SortitionInfo) SLAContractArray;
     
-  }
+    
+
+    // check whether it is a registered provider
+    modifier checkProvider(address _provider){
+        require(providerCrowd[_provider].registered);
+        _;
+    }    
+
+
 
 
 
@@ -26,14 +53,14 @@ contract CloudAuction {
     using SortingMethods for uint[];
     uint[] bidArray;
 
-   // this function add the bids from different providers
+    // this function add the bids from different providers
     function addBids (uint[] memory _ArrayToAdd) public {
         for (uint i=0; i< _ArrayToAdd.length; i++){
             bidArray.push(_ArrayToAdd[i]);
         }
     }
 
-    function sortArray() public returns(uint[] memory){
+    function sortByPriceAscending() public returns(uint[] memory){
         bidArray = bidArray.heapSort();
         return bidArray;
     }
@@ -41,7 +68,7 @@ contract CloudAuction {
 
     /**
      * Provider Interface::
-     * This is for the provider to generate a SLA contract
+     * This is for the winner provider to generate a SLA contract
      * */
     function genSLAContract() 
         public 
@@ -52,9 +79,23 @@ contract CloudAuction {
         SLAContractPool[newSLAContract].valid = true; 
         emit SLAContractGen(msg.sender, now, newSLAContract);
         return newSLAContract;
-    }
+    }    
+
+
+
+
+
+
+
+
+
 
 }
+
+
+
+
+
 
 
 
