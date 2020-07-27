fea231db955d8f8f3e9c1f77969fd9c22a3401b7
ZeshunShi <44780304+ZeshunShi@users.noreply.github.com>
2020-07-28 01:05:24 +0200

    Update testReveal.sol

--- testReveal.sol
+++ testReveal.sol
@@ -58,18 +58,20 @@ contract AuctionManagement {
         // checkTimeBefore(revealEnd)
         // checkCustomer(msg.sender)
         // checkBidderNumber(bidderAddresses.length > 5 && customerAddresses.length == 1)
+        returns(uint)
     {
         require (_reservePrice != 0 && _customerPassword != 0);
         if(keccak256(abi.encodePacked(_reservePrice, _customerPassword)) == auctionItemStructs[msg.sender].sealedReservePrice){
             reservePrice = _reservePrice;
         }
+        return reservePrice;
     }
     
-    // address [] public revealedBidder;
-    // uint [] public revealedBids;
-    mapping(address => uint) public revealedBids;
+    address [] public revealedBidders;
+    uint [] public revealedBids;
+    // mapping(address => uint) public revealedBids;
     
-    function revealBids (uint _bid, uint _providerPassword)
+    function revealBids (string memory _providerName, uint _bid, uint _providerPassword)
         public
         payable
         // checkTimeAfter(bidEnd)
@@ -78,17 +80,44 @@ contract AuctionManagement {
         // checkBidderNumber(bidderAddresses.length > 5 && customerAddresses.length == 1)
     {
         require (_bid != 0 && _providerPassword != 0);
+        require (keccak256(abi.encodePacked(bidStructs[msg.sender].providerName)) == keccak256(abi.encodePacked(_providerName)));
         if(keccak256(abi.encodePacked(_bid, _providerPassword)) == bidStructs[msg.sender].sealedBid){
-            revealedBids[msg.sender] = _bid;
-            // revealedBidder.push(msg.sender);
-            // revealedBids.push(_bid);
-        }      
-        
-        // for (uint i=0; i < bidderAddresses.length; i++) {
-        //     totalBids += bidStructs[bidderAddresses[i]];
-        //     if(keccak256(abi.encodePacked(_bid, _providerPassword)) == bidStructs[msg.sender].sealedReservePrice){
-        //     revealedBids[msg.sender] = _bid;
-        // }
-    // }
+            // revealedBids[msg.sender] = _bid;
+            revealedBidders.push(msg.sender);
+            revealedBids.push(_bid);
+        }
     }
+    
+    function test1() public view returns(address[] memory){
+        return revealedBidders;
+    }
+    
+    function test2() public view returns(uint[] memory){
+        return revealedBids;
+    }
+        
+    // function sortBidsByAscending () 
+    //     public
+    //     // checkTimeAfter(bidEnd)
+    //     // checkTimeBefore(revealEnd)
+    //     // checkProvider(msg.sender)
+    //     // checkBidderNumber(revealedBidders.length > 5 && revealedBids.length > 5)
+    //     returns(uint[] memory)
+    // {
+    //     require(revealedBidders.length > 5 && revealedBids.length > 5,"Insufficient reveledBids/revealedBidders to sort");
+    //     bool swapped;
+    //     uint i;
+    //     uint j;  
+    //     for (uint i=0; i < revealedBids.length - 1; i++) {
+    //         swapped = false;
+    //         for (j =0; j < revealedBids.length-i-1; j++){
+    //             if (revealedBids[j] > revealedBids[j+1]){
+    //                 (revealedBids[j], revealedBids[j+1]) = (revealedBids[j+1], revealedBids[j]);
+    //                 (revealedBidders[j], revealedBidders[j+1]) = (revealedBidders[j+1], revealedBidders[j]);
+    //                 swapped = true;
+    //             }
+    //         }
+    //             if(swapped==false) break;
+    //     }
+    // }
 }
